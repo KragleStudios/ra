1811ba7c0f2ba0343b1e9de2adf5976c08dbb654
thelastpenguin <thelastpenguin212@gmail.com>
2016-07-16 01:34:52 -0700

    better random colors

--- lua/ra/modules/print.lua
+++ lua/ra/modules/print.lua
@@ -1,13 +1,16 @@
 local src_name_cache = {}
 local color_cache = {}
 
+local currentColor = 0
+
 local function print(...)
 	local src = debug.getinfo(2, 'S')
 	if not src_name_cache[src.short_src] then
 		src_name_cache[src.short_src] = string.GetFileFromFilename(src.short_src)
 	end
 	if not color_cache[src.short_src] then
-		color_cache[src.short_src] = HSVToColor(math.random(0, 10000000), 1, 1)
+		color_cache[src.short_src] = HSVToColor(currentColor, 1, 1)
+		currentColor = currentColor + math.random(30, 100)
 	end
 
 	MsgC(color_cache[src.short_src], src_name_cache[src.short_src] .. ':' .. src.linedefined .. ' ')
